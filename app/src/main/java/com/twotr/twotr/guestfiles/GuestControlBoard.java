601fc90e11a5b7634a65e0418ee59b90547c7436
quaticstech <quaticstech@gmail.com>
2018-02-26 03:24:01 +0530

    Guest Changes.

--- app/src/main/java/com/twotr/twotr/guestfiles/GuestControlBoard.java
+++ app/src/main/java/com/twotr/twotr/guestfiles/GuestControlBoard.java
@@ -13,15 +13,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.allattentionhere.fabulousfilter.AAH_FabulousFragment;
 import com.twotr.twotr.R;
@@ -53,10 +56,10 @@ String searchque="ameer";
     String Stoken,Sid;
     String[] aryGrade = {
     };
+   public View ftview;
     EditText ETsubject_name;
     MyFabFragment dialogFrag;
     FloatingActionButton  fab2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,6 +100,23 @@ ETsubject_name=findViewById(R.id.subject_name);
                 dialogFrag.show(getSupportFragmentManager(), dialogFrag.getTag());
             }
         });
+
+//        listViewguest.setOnScrollListener(new EndlessScrollListener() {
+//            @Override
+//            public void onLoadMore(int page, int totalItemsCount) {
+//
+//
+//                schedule_list_url = Global_url_twotr.Guest_list_api+searchque+"&page=1&size="+page+"0" ;
+//                new ScheduleAsyncList().execute(schedule_list_url);
+//
+//
+//
+//            }
+//
+//        });
+
+
+
     }
 
 
@@ -224,6 +244,9 @@ DataInputStream inputStream;
                 price.put("max",1);
                 auth.put("price",price);
                 auth.put("ratings", "0");
+                auth.put("timezone", "Asia/Kuwait");
+
+
                 printout = new DataOutputStream(connection.getOutputStream ());
                 printout.writeBytes(auth.toString());
                 printout.flush ();
@@ -259,13 +282,13 @@ DataInputStream inputStream;
 
                         //  ListSubject.add(Skind);
                     }
-                    try {
-                        JSONObject jlocati=finalObject.getJSONObject("location");
-                        catego.setLat(jlocati.getString("lat"));
-                        catego.setLng(jlocati.getString("lng"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+//                    try {
+//                        JSONObject jlocati=finalObject.getJSONObject("location");
+//                        catego.setLat(jlocati.getString("lat"));
+//                        catego.setLng(jlocati.getString("lng"));
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
 
                     milokilo.add(catego);
                 }
@@ -296,6 +319,7 @@ DataInputStream inputStream;
 
                 Schedule_class adapter = new Schedule_class(GuestControlBoard.this, R.layout.guest_dasboard_list, ScheduleMode);
                 listViewguest.setAdapter(adapter);
+
                 avi.hide();
 //                listViewguest.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //                        @Override
@@ -330,7 +354,7 @@ DataInputStream inputStream;
 //                            startActivity(intent);
 //                        }
 //                    });
-                    adapter.notifyDataSetChanged();
+                   adapter.notifyDataSetChanged();
 
 
 
@@ -415,4 +439,75 @@ DataInputStream inputStream;
         Log.d("aah_animation", "onCloseAnimationEnd: ");
 
     }
+
+
+//    public abstract class EndlessScrollListener implements AbsListView.OnScrollListener {
+//
+//        // The minimum amount of items to have below your current scroll position
+//        // before loading more.
+//        private int visibleThreshold = 2;
+//        // The current offset index of data you have loaded
+//        private int currentPage = 0;
+//        // The total number of items in the dataset after the last load
+//        private int previousTotalItemCount = 0;
+//        // True if we are still waiting for the last set of data to load.
+//        private boolean loading = true;
+//        // Sets the starting page index
+//        private int startingPageIndex = 0;
+//
+//        public EndlessScrollListener() {
+//        }
+//
+//        public EndlessScrollListener(int visibleThreshold) {
+//            this.visibleThreshold = visibleThreshold;
+//        }
+//
+//        public EndlessScrollListener(int visibleThreshold, int startPage) {
+//            this.visibleThreshold = visibleThreshold;
+//            this.startingPageIndex = startPage;
+//            this.currentPage = startPage;
+//        }
+//
+//        // This happens many times a second during a scroll, so be wary of the code you place here.
+//        // We are given a few useful parameters to help us work out if we need to load some more data,
+//        // but first we check if we are waiting for the previous load to finish.
+//        @Override
+//        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+//            // If the total item count is zero and the previous isn't, assume the
+//            // list is invalidated and should be reset back to initial state
+//            if (totalItemCount < previousTotalItemCount) {
+//                this.currentPage = this.startingPageIndex;
+//                this.previousTotalItemCount = totalItemCount;
+//                if (totalItemCount == 0) {
+//                    this.loading = true;
+//                }
+//            }
+//
+//            // If it’s still loading, we check to see if the dataset count has
+//            // changed, if so we conclude it has finished loading and update the current page
+//            // number and total item count.
+//            if (loading && (totalItemCount > previousTotalItemCount)) {
+//                loading = false;
+//                previousTotalItemCount = totalItemCount;
+//                currentPage++;
+//            }
+//
+//            // If it isn’t currently loading, we check to see if we have breached
+//            // the visibleThreshold and need to reload more data.
+//            // If we do need to reload some more data, we execute onLoadMore to fetch the data.
+//            if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+//                onLoadMore(currentPage + 1, totalItemCount);
+//                loading = true;
+//            }
+//        }
+//
+//        // Defines the process for actually loading more data based on page
+//        public abstract void onLoadMore(int page, int totalItemsCount);
+//
+//        @Override
+//        public void onScrollStateChanged(AbsListView view, int scrollState) {
+//            // Don't take any action on changed
+//
+//        }
+//    }
 }
